a13f0900a3e498c33898b301e7efe46a67e13bc2
₳₳₳₳₳₳₳ <dwomeco2@github.fake>
2023-03-18 18:05:48 +0800

    fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}",
